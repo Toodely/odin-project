36e7f91cba25a71f7815674933e3e0785eb78882
Sam Valentine <sam@o-mills.co.uk>
2021-07-06 10:17:58 +0100

    Added button to webpage to playgame, wrapped script in a playGame() function for this. Very basic style on page

--- rock-paper-scissors/scripts.js
+++ rock-paper-scissors/scripts.js
@@ -1,42 +1,46 @@
-let playerScore = 0;
-let computerScore = 0;
-let draws = 0;
+function playGame() {
 
-for (roundNo = 1; roundNo < 6; roundNo ++) {
+    let playerScore = 0;
+    let computerScore = 0;
+    let draws = 0;
 
-    function computerPlay() {
-        let computerHand = ["Rock", "Paper", "Scissors"]
-        let computerRandomHand = Math.floor(Math.random() * computerHand.length);
-        return(computerHand[computerRandomHand]);
+    for (roundNo = 1; roundNo < 6; roundNo ++) {
+
+        function computerPlay() {
+            let computerHand = ["Rock", "Paper", "Scissors"]
+            let computerRandomHand = Math.floor(Math.random() * computerHand.length);
+            return(computerHand[computerRandomHand]);
+        }
+
+        const computerChoice = computerPlay();
+            
+        const playerChoice = prompt("It's round "+ roundNo + ". " + "Please enter Rock, Paper or Scissors.");
+
+        if (computerChoice == "Rock" && playerChoice == "Rock" ||
+            computerChoice == "Paper" && playerChoice == "Paper" ||
+            computerChoice == "Scissors" && playerChoice == "Scissors") {
+                alert("You chose " + playerChoice + " and the computer chose " + computerChoice + ". It's a draw!")
+                draws ++;
+            } 
+        else if 
+            (computerChoice == "Rock" && playerChoice == "Scissors" ||
+            computerChoice == "Paper" && playerChoice == "Rock" ||
+            computerChoice == "Scissors" && playerChoice == "Paper") {
+                alert("You chose " + playerChoice + " and the computer chose " + computerChoice + ". The computer wins!")
+                computerScore ++;
+            }
+        else if 
+            (computerChoice == "Rock" && playerChoice == "Paper" ||
+            computerChoice == "Paper" && playerChoice == "Scissors" ||
+            computerChoice == "Scissors" && playerChoice == "Rock") {
+                alert("You chose " + playerChoice + " and the computer chose " + computerChoice + ". You win!")
+                playerScore ++;
+            }
+
+        if (roundNo == 5) {
+                alert("Player wins: " + playerScore + " Computer wins: " + computerScore + " Draws: " + draws);
+            }
+
     }
-
-    const computerChoice = computerPlay();
-        
-    const playerChoice = prompt("It's round "+ roundNo + ". " + "Please enter Rock, Paper or Scissors.");
-
-    if (computerChoice == "Rock" && playerChoice == "Rock" ||
-        computerChoice == "Paper" && playerChoice == "Paper" ||
-        computerChoice == "Scissors" && playerChoice == "Scissors") {
-            alert("You chose " + playerChoice + " and the computer chose " + computerChoice + ". It's a draw!")
-            draws ++;
-        } 
-    else if 
-        (computerChoice == "Rock" && playerChoice == "Scissors" ||
-        computerChoice == "Paper" && playerChoice == "Rock" ||
-        computerChoice == "Scissors" && playerChoice == "Paper") {
-            alert("You chose " + playerChoice + " and the computer chose " + computerChoice + ". The computer wins!")
-            computerScore ++;
-        }
-    else if 
-        (computerChoice == "Rock" && playerChoice == "Paper" ||
-        computerChoice == "Paper" && playerChoice == "Scissors" ||
-        computerChoice == "Scissors" && playerChoice == "Rock") {
-            alert("You chose " + playerChoice + " and the computer chose " + computerChoice + ". You win!")
-            playerScore ++;
-        }
-
-    if (roundNo == 5) {
-            alert("Player wins: " + playerScore + " Computer wins: " + computerScore + " Draws: " + draws);
-        }
 
 }
