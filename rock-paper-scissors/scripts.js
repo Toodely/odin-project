fbf9ef7cde0875cc09b4558c5b418552abd50503
Sam Valentine <sam@o-mills.co.uk>
2021-07-06 09:37:19 +0100

    Added player choice and if/else system to check who wins, wrapped in a for loop to play five rounds on page load

--- rock-paper-scissors/scripts.js
+++ rock-paper-scissors/scripts.js
@@ -1,7 +1,32 @@
-function computerPlay() {
-    let computerHand = ["Rock", "Paper", "Scissors"]
-    let computerRandomHand = Math.floor(Math.random() * computerHand.length);
-    return(computerHand[computerRandomHand]);
-}
 
-let computerChoice = computerPlay();
+for (roundNo = 0; roundNo < 5; roundNo ++) {
+
+    function computerPlay() {
+        let computerHand = ["Rock", "Paper", "Scissors"]
+        let computerRandomHand = Math.floor(Math.random() * computerHand.length);
+        return(computerHand[computerRandomHand]);
+    }
+
+    const computerChoice = computerPlay();
+        
+    const playerChoice = prompt("Please enter Rock, Paper or Scissors.");
+
+        if (computerChoice == "Rock" && playerChoice == "Rock" ||
+        computerChoice == "Paper" && playerChoice == "Paper" ||
+        computerChoice == "Scissors" && playerChoice == "Scissors") {
+            alert("You chose " + playerChoice + " and the computer chose " + computerChoice + ". It's a draw!")
+        } 
+    else if 
+        (computerChoice == "Rock" && playerChoice == "Scissors" ||
+        computerChoice == "Paper" && playerChoice == "Rock" ||
+        computerChoice == "Scissors" && playerChoice == "Paper") {
+            alert("You chose " + playerChoice + " and the computer chose " + computerChoice + ". The computer wins!")
+        }
+    else if 
+        (computerChoice == "Rock" && playerChoice == "Paper" ||
+        computerChoice == "Paper" && playerChoice == "Scissors" ||
+        computerChoice == "Scissors" && playerChoice == "Rock") {
+            alert("You chose " + playerChoice + " and the computer chose " + computerChoice + ". You win!")
+        }
+
+}
